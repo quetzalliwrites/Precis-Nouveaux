7b114595c935b7da11f360f8ebf112fbd1f73d1e
Robert Olson <robert@highest.technology>
2017-05-21 13:08:48 -0700

    json read quick and dirty

--- router/main.js
+++ router/main.js
@@ -5,7 +5,18 @@ var articles = require("../services/hello-world.js");
 module.exports = function(app) {
 
      app.get('/phase3',function(req,res){
-          var articleOne = articles.getArticleOne();
+         
+         
+          var fs = require("fs");
+          console.log("\n *STARTING* \n");
+// Get content from file
+          var contents = fs.readFileSync("Row1.json");
+// Define to JSON type
+          var jsonContent = JSON.parse(contents);
+// Get Value from JSON
+          var a = jsonContent[0].TEXT;
+         
+          var articleOne = a;
           var articleTwo = articles.getArticleTwo();
           var articleThree = articles.getArticleThree();
           
